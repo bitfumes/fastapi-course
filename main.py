64e93331618d26cc4e519fe1009005dc695c6871
Sarthak Shrivastava <sarthak@Sarthaks-MacBook-Air.local>
2021-02-03 09:52:40 +0530

    request body

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+
+
 app = FastAPI()
 
 
@@ -27,3 +30,14 @@ def show(id: int):
 def comments(id, limit=10):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
