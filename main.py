6b8910ac0b49e167fe966562df30b976aa2bb9fa
Sarthak Shrivastava <sarthak@Sarthaks-MacBook-Air.local>
2021-02-03 09:27:12 +0530

    query parameters

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
 
 
 @app.get('/blog/{id}')
-def show(id):
+def show(id: int):
     # fetch blog with id = id
     return {'data': id}
 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
