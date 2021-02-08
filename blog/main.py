de95368a3507433a516dc6e20dbb8311371b0486
Sarthak Shrivastava <sarthak@Sarthaks-MacBook-Air.local>
2021-02-08 18:10:47 +0530

    create model and table

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from . import schemas
+from . import schemas, models
+from .database import engine
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 
 @app.post('/blog')
---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
