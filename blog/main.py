5a5baf02b0f07946def4efc6a1c49067ef350a6c
Sarthak Shrivastava <sarthak@Sarthaks-MacBook-Air.local>
2021-02-08 18:23:14 +0530

    get blogs from database

--- blog/main.py
+++ blog/main.py
@@ -23,3 +23,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
