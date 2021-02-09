cdf6eeb93f68ae99ace9014eea3d4751a719a743
sarthaksavvy <bitfumes@gmail.com>
2021-02-09 14:04:45 +0530

    delete a blog

--- blog/main.py
+++ blog/main.py
@@ -25,6 +25,14 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id ==
+                                 id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
