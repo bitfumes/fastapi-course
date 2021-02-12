7ad7e70b60ff944505e1e989bc76d0b1eef85938
sarthaksavvy <bitfumes@gmail.com>
2021-02-12 12:50:34 +0530

    response model

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -51,13 +52,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+
+    class Config():
+        orm_mode = True
