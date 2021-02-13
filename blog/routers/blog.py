5a167c873a748618222990c8fdae936d021c203f
sarthaksavvy <bitfumes@gmail.com>
2021-02-13 20:44:55 +0530

    api router path operators

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,16 +2,22 @@ from typing import List
 from fastapi import APIRouter,Depends,status,HTTPException
 from .. import schemas, database, models
 from sqlalchemy.orm import Session
-router = APIRouter()
+
+
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blogs']
+)
+
 get_db = database.get_db
 
-@router.get('/blog', response_model=List[schemas.ShowBlog],tags=['blogs'])
+@router.get('/', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body,user_id=1)
     db.add(new_blog)
@@ -19,7 +25,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -32,7 +38,7 @@ def destroy(id, db: Session = Depends(get_db)):
     return 'done'
 
 
-@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -45,7 +51,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog,tags=['blogs'])
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -4,11 +4,15 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter,Depends,status,HTTPException
 from ..hashing import Hash
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/user",
+    tags=['Users']
+)
+
 get_db = database.get_db
 
 
-@router.post('/user', response_model=schemas.ShowUser,tags=['users'])
+@router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User,db: Session = Depends(get_db)):
     new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
     db.add(new_user)
@@ -16,7 +20,7 @@ def create_user(request: schemas.User,db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+@router.get('/{id}',response_model=schemas.ShowUser)
 def get_user(id:int,db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
